b74f10d3819bc81b9d6b440b127f423a216cea5b
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-01-14 00:36:37 +0500

    repairing about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,11 +2,27 @@
 import "./About.css";
 
 // Images
+import myImage from "../../../../../Images/Sardorbek2007.jpg";
 
 function About() {
   return (
     <section className="about" id="about">
-      <div className="aboutMe"></div>
+      <div className="aboutMe">
+        <div className="column">
+          <div className="myImage">
+            <img src={myImage} alt="Sardorbek Olimov's image" />
+          </div>
+          <div className="myName">
+            <h4>Sardorbek Olimov</h4>
+          </div>
+          <div className="frontendDev">
+            <h2>Frontend Developer</h2>
+          </div>
+          <div className="myInfo">
+            
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
