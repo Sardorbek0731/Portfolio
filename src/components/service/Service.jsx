1aa3ba24642e89f9a4235d8ec8046ed52b865bff
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-02-21 23:43:16 +0500

    repairing service item

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,11 +1,69 @@
 // CSS
 import "./Service.css";
 
+// Icons
+import files from "../../assets/icons/files.png";
+import paint from "../../assets/icons/paint.png";
+import apps from "../../assets/icons/apps.png";
+import bot from "../../assets/icons/bot.png";
+
 function Service() {
   return (
     <section className="service">
       <div className="container">
-        <h1>Service</h1>
+        <div className="title">
+          <h1>Service</h1>
+        </div>
+        <div className="services flex alignCenter justifyBetween wf-100">
+          <div className="serviceItem">
+            <div className="serviceItem_title">
+              <img src={files} alt="Pencil image" />
+              <h3>Web Programm</h3>
+            </div>
+            <div className="serviceItem_body">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                aliquam delectus.
+              </p>
+            </div>
+          </div>
+          <div className="serviceItem">
+            <div className="serviceItem_title">
+              <img src={apps} alt="Pencil image" />
+              <h3>Mobile Apps</h3>
+            </div>
+            <div className="serviceItem_body">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                aliquam delectus.
+              </p>
+            </div>
+          </div>
+          <div className="serviceItem">
+            <div className="serviceItem_title">
+              <img src={paint} alt="Pencil image" />
+              <h3>UI / UX Design</h3>
+            </div>
+            <div className="serviceItem_body">
+              <p>
+                Lorem dolor sit amet consectetur adipisicing elit. Maiores
+                delectus odio.
+              </p>
+            </div>
+          </div>
+          <div className="serviceItem">
+            <div className="serviceItem_title">
+              <img src={bot} alt="Pencil image" />
+              <h3>Telegram Bots</h3>
+            </div>
+            <div className="serviceItem_body">
+              <p>
+                Lorem ipsum consectetur adipisicing. Maiores aliquam delectus
+                odio a odit dolores.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
