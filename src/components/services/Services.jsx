1df08d42747141355d8fb085fbefd433f195bd34
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-03-12 23:23:50 +0500

    repaired slider buttons animation

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -12,8 +12,12 @@ import right from "../../assets/icons/right.png";
 // react-i18next
 import { useTranslation } from "react-i18next";
 
+// Hooks
+import { useState } from "react";
+
 function Service() {
   const { t } = useTranslation();
+  const [sliderIndex, setSliderIndex] = useState(0);
 
   const services = [
     {
@@ -25,6 +29,11 @@ function Service() {
       icon: files,
       title: "Web Sites",
       description: "I can build a website for you in no time.",
+    },
+    {
+      icon: paint,
+      title: "UI / UX Design",
+      description: "I can create a copy of the website layout.",
     },
     {
       icon: paint,
@@ -50,7 +59,10 @@ function Service() {
           <h1>Service</h1>
         </div>
         <div className="services flex alignCenter wf-100">
-          <div className="servicesItems flex alignCenter justifyBetween">
+          <div
+            className="servicesItems flex alignCenter justifyBetween"
+            style={{ transform: `translateX(${-sliderIndex * 16.7}rem)` }}
+          >
             {services.map((item, index) => {
               return (
                 <div className="serviceItem" key={index}>
@@ -68,14 +80,26 @@ function Service() {
 
           <div className="slider flex alignCenter justifyBetween">
             <div className="slideBlur slideBlur_left flex alignCenter">
-              <div className="leftBtn slideBtn flex alignCenter justifyCenter">
+              <button
+                className="leftBtn slideBtn flex alignCenter justifyCenter"
+                disabled={sliderIndex == 0 ? true : false}
+                onClick={() => {
+                  setSliderIndex(sliderIndex - 1);
+                }}
+              >
                 <img src={left} alt="Left arrow" />
-              </div>
+              </button>
             </div>
             <div className="slideBlur slideBlur_right flex alignCenter">
-              <div className="rightBtn slideBtn flex alignCenter justifyCenter">
+              <button
+                className="rightBtn slideBtn flex alignCenter justifyCenter"
+                disabled={sliderIndex > services.length - 4 ? true : false}
+                onClick={() => {
+                  setSliderIndex(sliderIndex + 1);
+                }}
+              >
                 <img src={right} alt="Right arrow" />
-              </div>
+              </button>
             </div>
           </div>
         </div>
